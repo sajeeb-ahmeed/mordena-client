54547472fd96ba52ae07ce478259c81a0e543951
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-05-06 21:29:56 +0600

    quotes info added b

--- src/Pages/Home/Latest/LatesInfo.js
+++ src/Pages/Home/Latest/LatesInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion } from 'react-bootstrap';
+import { Accordion, Card, Carousel } from 'react-bootstrap';
 import { GiCargoShip, GiCommercialAirplane } from 'react-icons/gi';
 import { FaTruckLoading, FaGlobeEurope } from 'react-icons/fa';
 
@@ -38,8 +38,104 @@ const LatesInfo = () => {
                         </Accordion.Item>
                     </Accordion>
                 </div>
-                <div className="col-sm-12 col-md-6 col-12 col-xs-12">
+                <div className="col-sm-12 col-md-6 col-12 col-xs-12 mx-auto">
+                    <h4 className='my-5 fw-bold'>WHAT MAKES US SPECIAL?</h4>
+                    <div className="row">
+                        <div className="col-md-6 col-sm-12 col-12">
+                            <Carousel className='cards'>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100   imgCards"
+                                        src={`https://hatil.com/sites/default/files/styles/hatil_product_list_crop/public/Marseille-183.jpg`}
+                                        alt="First slide"
+                                    />
+                                    <Carousel.Caption>
 
+                                        <p className='text-dark'>firms in the United Kingdom.</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100   imgCards"
+                                        src={`https://hatil.com/sites/default/files/styles/hatil_product_list_crop/public/Rennesa-104.jpg`}
+                                        alt="Second slide"
+                                    />
+                                    <Carousel.Caption>
+
+                                        <p className='text-dark'>Carefully-chosen raw material,.</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100 imgCards"
+                                        src={`https://hatil.com/sites/default/files/Sofa_0.jpg`}
+                                        alt="Third slide"
+                                    />
+                                    <Carousel.Caption>
+
+                                        <p className='text-dark'>one of the most experienced</p>
+                                    </Carousel.Caption>
+
+                                </Carousel.Item>
+                            </Carousel>
+                            <Card className='cards-special'>
+                                <Card.Body>
+                                    <Card.Title className='text-uppercase'>11 Times Old Furniture Gained New Life</Card.Title>
+                                    <Card.Text>
+                                        Coordinating and supervising work performed trades and outsite supplier for quality, completeness,schedule.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col-md-6 col-sm-12 col-12">
+                            <Carousel className='cards'>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100   imgCards "
+                                        src={`https://hatil.com/sites/default/files/Open-Shelves.jpg`}
+                                        alt="First slide"
+                                    />
+                                    <Carousel.Caption>
+
+                                        <p className='text-dark'>Coordinating and supervising work </p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100   imgCards"
+                                        src={`https://hatil.com/sites/default/files/Chair_1.jpg`}
+                                        alt="Second slide"
+                                    />
+                                    <Carousel.Caption>
+
+                                        <p className='text-dark'>the bulk of
+                                            the card's content.</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100   imgCards"
+                                        src={`https://hatil.com/sites/default/files/File-Rack.jpg`}
+                                        alt="Third slide"
+                                    />
+
+                                    <Carousel.Caption>
+
+                                        <p className='text-dark'>for quality, completeness,schedule.</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            </Carousel>
+                            <Card className='cards-special'>
+                                <Card.Body>
+                                    <Card.Title className='text-uppercase'>How to restore old wodden furniture</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

08a876154d2698657fd7cb8d0a2b1bdbab6c6575
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-05-07 10:56:31 +0600

    user handdle

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import logo from '../../../images/logo.png'
-import { MdAddIcCall, MdOutgoingMail } from 'react-icons/md'
+import logo from '../../../images/logos.png'
+import { MdAddIcCall, MdOutgoingMail } from 'react-icons/md';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/firebase.init';
+import { signOut } from 'firebase/auth';
+
+
+
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" sticky='top' bg="light" variant="light">
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
                 <Container className='text-center'>
                     <Navbar.Brand as={Link} to="/">
 
@@ -16,11 +27,11 @@ const Header = () => {
                         <Nav className="me-auto">
 
                             <div className=" flex-column">
-                                <a className='me-lg-5 mb-4 mb-lg-0 text-dark' href="tel:+8801748402018">
-                                    <MdAddIcCall size="24" className="nav-linker text-dark me-2" />
+                                <a className='me-lg-5 mb-4 mb-lg-0 text-light' href="tel:+8801748402018">
+                                    <MdAddIcCall size="24" className="nav-linker text-light me-2" />
                                     +01748402018
                                 </a>
-                                <span className='text-dark' onClick={() => window.location = 'mailto:sajeeb.web@gmail.com'}> <MdOutgoingMail size="24" className="nav-linker text-dark" />
+                                <span className='text-light' onClick={() => window.location = 'mailto:sajeeb.web@gmail.com'}> <MdOutgoingMail size="24" className="nav-linker text-light" />
                                     Sajeeb.web@gmail.com</span>
                             </div>
 
@@ -30,13 +41,28 @@ const Header = () => {
                         </Nav>
                         <Nav className="ms-auto">
                             <Nav.Link as={Link} to="/">Home</Nav.Link>
+
+                        </Nav>
+                        <Nav>
                             <Nav.Link as={Link} to="about">About</Nav.Link>
+                            {
+                                user && <>
+                                    <Nav.Link as={Link} to="addservice">Add</Nav.Link>
+                                    <Nav.Link as={Link} to="manage">Manage</Nav.Link>
+                                    <Nav.Link as={Link} to="orders">Orders</Nav.Link>
+                                </>
+                            }
+                            {
+                                user ?
+                                    <>
+                                        <Nav.Link onClick={handleSignOut}> Log Out</Nav.Link>
+                                        {/* <img className='text-center' style={{ width: '40px', height: "40px", borderRadius: "50%", marginLeft: "5px" }} a src={user?.photoURL} alt="User" /> */}
 
-
-
-                            <Nav.Link as={Link} to="login">
-                                Login
-                            </Nav.Link>
+                                    </>
+                                    :
+                                    <Nav.Link as={Link} to="/login">
+                                        Login
+                                    </Nav.Link>}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- src/Pages/Verification/RequireAuth/RequireAuth.js
+++ src/Pages/Verification/RequireAuth/RequireAuth.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../Firebase/firebase.init';
 
 import Loading from '../../Shared/Loading/Loading';
@@ -8,10 +9,8 @@ import Loading from '../../Shared/Loading/Loading';
 const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
-
-
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     if (loading) {
-        console.log('loading ');
         return <Loading></Loading>
     }
 
@@ -19,7 +18,22 @@ const RequireAuth = ({ children }) => {
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
-
+    if (user.providerData[0]?.providerId === 'password' && !user.emailVerified) {
+        return <div className='text-center mt-5'>
+            <h3 className='text-danger'>Your Email is not verified!!</h3>
+            <h5 className='text-success'> Please Verify your email address</h5>
+            <button
+                className='btn btn-primary'
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('Sent email');
+                }}
+            >
+                Send Verification Email Again
+            </button>
+            <ToastContainer></ToastContainer>
+        </div>
+    }
 
     return children;
 };
